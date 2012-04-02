7f8b4f07a5d3341f7ec997f84609172257c7325f
Jonathan Mellman <jonathan.mellman@furman.edu>
2012-04-02 14:38:26 -0400

    Added body for Slope1Ramp

---
+++ SkySlider/SkySlider/Player.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GraphicsToolkit.Graphics;
+using GraphicsToolkit.Physics._3D.Bodies;
+using Microsoft.Xna.Framework;
+
+namespace SkySlider
+{
+    public class Player
+    {
+        private ThirdPersonCamera cam;
+        private SphereBody sphereBody;
+
+        public Player(Vector3 initialPlayerPosition)
+        {
+            cam = new ThirdPersonCamera(initialPlayerPosition, 0.7f, 1f);
+            sphereBody = new SphereBody(initialPlayerPosition, Vector3.Zero, 1f, 0.18f);
+        }
+
+        public ThirdPersonCamera Cam
+        {
+            get
+            {
+                return cam;
+            }
+        }
+
+        public SphereBody Body
+        {
+            get
+            {
+                return sphereBody;
+            }
+        }
+
+        public void Update(GameTime g)
+        {
+            cam.TargetPos = sphereBody.Pos;
+            cam.Update(g);
+        }
+    }
+}
